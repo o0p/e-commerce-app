5727ddb98fe0f8e7ce12fe9367809dfc1f6b6222
Russell <russelldev@protonmail.com>
2021-03-29 23:53:47 +0100

    add Product.jsx

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+function Product() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Product;
