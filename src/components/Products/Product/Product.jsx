e28b35b444b2c2392dea1af0db1f4b9f2a7bf69d
Russell <russelldev@protonmail.com>
2021-03-29 23:56:17 +0100

    add CardMedia to Product.jsx

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
+import classes from '*.module.css';
 
 function Product() {
     return (
-        <div>
-            
-        </div>
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image={} />
+        </Card>
     )
 }
 
