d0e15c63b768af430c464603a5c61cc8057abbb2
Russell <russelldev@protonmail.com>
2021-03-29 23:59:42 +0100

    Add Typography to Grid

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Grid from '@material-ui/core';
+import Grid, { Typography } from '@material-ui/core';
+import classes from '*.module.css';
 
 const products = [
   { id: 1, name: 'Shoes', description: 'Running shoes.' },
@@ -12,6 +13,13 @@ const Products = () => {
       {products.map((product) => {
         <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
           <Products product={product} />
+          <CardContent>
+            <div className={classes.CardContent}>
+              <Typography variant="h5" gutterBottom>
+                {product.name}
+              </Typography>
+            </div>
+          </CardContent>
         </Grid>;
       })}
     </Grid>
